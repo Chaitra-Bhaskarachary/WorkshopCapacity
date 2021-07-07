868e2b2b5e8784887b471104758f1de2f89a9450
Chaitra Bhaskarachary <chaitra.bhaskarachary@auto1.com>
2021-07-07 17:57:36 +0200

    Updated code

--- capacity/forms.py
+++ capacity/forms.py
@@ -11,19 +11,5 @@ class WorkshopCapacityForm(forms.ModelForm):
         model = WorkshopCapacity
   
         # specify fields to be used
-        fields = [
-            "admin_id",
-            "admin_name",
-            "calender_week",
-            "entry_check",
-            "exit_check",
-            "retail_ready"
-        ]
-        labels = {
-            'admin_id':'Admin ID',
-            'admin_name':'Admin Name',
-            'calender_week':'Calender Week',
-            'entry_check':'Entry Check',
-            'exit_check':'Exit Check',
-            'retail_ready':'Retail Ready'
-        }
+        fields = "__all__"
+      
--- workshop_capacity/urls.py
+++ workshop_capacity/urls.py
@@ -4,6 +4,6 @@ from capacity import views
 urlpatterns = [
     path('', views.entry_form,name='entry_insert'), # get and post req. for insert operation
     path('<int:id>/', views.entry_form,name='entry_update'), # get and post req. for update operation
-    path('delete/<int:id>/',views.entry_delete,name='entry_delete'),
-    path('list/',views.entry_list,name='entry_list') # get req. to retrieve and display all records
+    path('delete/<int:id>',views.entry_delete,name='entry_delete'),
+    path('entry/list/',views.entry_list,name='entry_list') # get req. to retrieve and display all records
 ]
